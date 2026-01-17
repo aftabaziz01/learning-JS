30618a4f9c1e0fae9bd66d4e9dd273bdba97b937
aftab.aziz <aftab.aziz@teknomee.com>
2026-01-18 02:37:19 +0530

    map

---
+++ map-filter-reduce.js
@@ -0,0 +1,26 @@
+// const arr = [5, 1, 3, 2, 6]
+
+// // Double - [10, 2, 6, 4, 12]
+
+// // Triple - [15, 3, 9, 6, 18]
+
+// // Binary - ["101", "1", "11", "10", "110"]
+
+// function double(x) {
+//     return x*2;
+// }
+// // const output = arr.map(double);
+// // console.log(output);
+
+// // function triple(x) {
+// //     return x*3;
+// // }
+// // const output = arr.map(triple);
+// // console.log(output);
+
+// const output= arr.map(function binary(x) {
+//     return x.toString(2);
+// });
+
+// console.log(output);
+
